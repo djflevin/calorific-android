d35a9517aed4f632413dea33e6103a9aae02f928
brightshock <80862052+brightshock@users.noreply.github.com>
2023-01-28 00:07:27 +0000

    Basic calorie count displayed

--- app/src/main/java/app/calorific/calorific/FoodJournalDayFragment.kt
+++ app/src/main/java/app/calorific/calorific/FoodJournalDayFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,8 +14,10 @@ import androidx.recyclerview.widget.RecyclerView
 import app.calorific.calorific.app.CalorificApplication
 import app.calorific.calorific.data.Meal
 import app.calorific.calorific.databinding.FragmentFoodJournalBinding
-import app.calorific.calorific.viewmodels.FoodJournalViewModel
 import app.calorific.calorific.viewmodels.FoodJournalDayViewModelFactory
+import app.calorific.calorific.viewmodels.FoodJournalViewModel
+import app.calorific.calorific.viewmodels.UserViewModel
+import app.calorific.calorific.viewmodels.UserViewModelFactory
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -23,9 +26,14 @@ class FoodJournalDayFragment(val date: ZonedDateTime) : Fragment() {
     private var _binding: FragmentFoodJournalBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: FoodJournalViewModel by viewModels {
+    private val foodJournalViewModel: FoodJournalViewModel by viewModels {
         FoodJournalDayViewModelFactory((requireActivity().application as CalorificApplication).repository, date.format(DateTimeFormatter.ISO_LOCAL_DATE))
     }
+    private val userViewModel: UserViewModel by activityViewModels {
+        UserViewModelFactory((requireActivity().application as CalorificApplication).repository)
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -75,23 +83,33 @@ class FoodJournalDayFragment(val date: ZonedDateTime) : Fragment() {
 
 
 
-        viewModel.foods.observe(viewLifecycleOwner) { foodList ->
-            adapter.submitList(
-                viewModel.mealNames.map { mealName ->
-                    Meal(
-                        name = mealName,
-                        foods = foodList.filter { it.instance.meal == mealName }
-                    )
-                }
-            )
-            adapter.currentList.sumOf { it.energy }
+        var calorieGoal: Int? = null
+        var meals: List<Meal> = listOf()
+
+        fun refreshCalorieCounter(){
+            when(calorieGoal){
+                null -> binding.caloriesLayout.visibility = View.GONE
+                else -> binding.caloriesLayout.visibility = View.VISIBLE
+            }
+            calorieGoal?.let { binding.remainingCaloriesValueTextView.text = String.format("%.0f", it - meals.sumOf { it.energy })}
+        }
+
+        userViewModel.caloriesGoal.observe(viewLifecycleOwner){
+            calorieGoal = it
+            refreshCalorieCounter()
         }
 
 
-        /*binding.caloriesRemainingTextView.text = String.format(
-            "%.0f",
-            2700 - mealList.sumOf { it.energy }
-        )*/
+        foodJournalViewModel.foods.observe(viewLifecycleOwner) { foodList ->
+            meals = foodJournalViewModel.mealNames.map { mealName ->
+                Meal(
+                    name = mealName,
+                    foods = foodList.filter { it.instance.meal == mealName }
+                )
+            }
+            adapter.submitList(meals)
+            refreshCalorieCounter()
+        }
 
 
     }
