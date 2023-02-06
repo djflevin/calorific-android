1bac03ebbe5b06a6dfed2e67be627afa4619529c
brightshock <80862052+brightshock@users.noreply.github.com>
2023-02-06 01:44:38 +0000

    Changed calories goal implementation in ProfileFragment

--- app/src/main/java/app/calorific/calorific/ProfileFragment.kt
+++ app/src/main/java/app/calorific/calorific/ProfileFragment.kt
@@ -1,17 +1,17 @@
 package app.calorific.calorific
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import app.calorific.calorific.app.CalorificApplication
+import app.calorific.calorific.databinding.DialogChangeCalorieGoalBinding
 import app.calorific.calorific.databinding.FragmentProfileBinding
 import app.calorific.calorific.viewmodels.UserViewModel
 import app.calorific.calorific.viewmodels.UserViewModelFactory
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ProfileFragment : Fragment() {
     private val TAG = "ProfileFragment"
@@ -39,15 +39,24 @@ class ProfileFragment : Fragment() {
         }
 
         viewModel.caloriesGoal.observe(viewLifecycleOwner){
-            if(!binding.calorieGoalEditText.isFocused){
-                it?.let { binding.calorieGoalEditText.setText(it.toString()) }
+            it?.let { binding.caloriesGoalValueTextView.text = it.toString() }
+        }
+
+        binding.caloriesGoalLayout.setOnClickListener { showCaloriesPopup() }
+    }
+
+    private fun showCaloriesPopup(){
+        val dialogBinding = DialogChangeCalorieGoalBinding.inflate(layoutInflater)
+        dialogBinding.caloriesGoalEditText.setText(viewModel.caloriesGoal.value.toString() ?: "")
+        MaterialAlertDialogBuilder(requireContext()).setView(dialogBinding.root)
+            .setTitle("Set Daily Calorie Goal")
+            .setPositiveButton("Save") {dialog, _ ->
+                viewModel.updateCalorieGoal(dialogBinding.caloriesGoalEditText.text.toString().toInt())
             }
-        }
-
-
-        binding.calorieGoalEditText.doAfterTextChanged {
-            viewModel.updateCalorieGoal(it.toString().toIntOrNull())
-        }
+            .setNegativeButton("Cancel") {dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
     override fun onDestroyView() {
